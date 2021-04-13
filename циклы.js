5073c6ac4e6d936654e8bf0333e311fd96129dcb
Dmitry <dmitry.gr.web@gmail.com>
2021-04-13 23:50:11 +0300

    циклы

---
+++ циклы.js
@@ -0,0 +1,33 @@
+"use strict";
+
+// let num = 50;
+
+// while (num <= 55) {
+//     console.log(num);
+//     num++;
+// }     //proverka uslovie i delaem komandu
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while (num < 55);  //delaem komandu i proveraem uslovie WHILE
+
+// for (let i = 1; i < 8; i++) {
+//     console.log(i);
+// } // cikl for
+
+
+// let num = 50;
+
+// for (let i = 1; i < 8; i++) {
+//     console.log(num);
+//     num++;
+// }
+
+for (let i = 1; i < 10; i++) {
+    if (i === 6) {
+        // break; //break prerivanie
+        continue; // prodolzhit do 10
+    }
+    console.log(i);
+}
