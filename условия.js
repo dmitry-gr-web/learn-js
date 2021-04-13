837645b2e4a22fbfe2def0bf899364c37e006750
Dmitry <dmitry.gr.web@gmail.com>
2021-04-13 23:20:56 +0300

    dobavil if else

---
+++ условия.js
@@ -0,0 +1,34 @@
+"use strict";
+// if (4 == 9) {
+//     console.log('ok!');
+// } else {
+//     console.log('Error');
+// }
+
+// const num = 50;
+// if (num < 49) {
+//     console.log('Errors');
+// } else if (num > 100) {
+//     console.log('Mnogo');
+// } else {
+//     console.log('ok');
+// }      // usloviya
+
+// (num === 50) ? console.log('ok'): console.log('Errors'); //ternarniy operator
+
+
+const num = 50;
+switch (num) {
+    case 49:
+        console.log('NEverno');
+        break;
+    case 100:
+        console.log('NEverno');
+        break;
+    case 50:
+        console.log('v tochku');
+        break;
+    default:
+        console.log('NE v etot raz');
+        break;
+} //switch case
